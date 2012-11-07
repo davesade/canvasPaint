d51753a4ed11ee66147cb7f2034c92ba5f1c6316
VictorHaydin <victor.haydin@gmail.com>
2012-11-07 13:37:54 +0200

    Reverted visited cache

--- js/index.js
+++ js/index.js
@@ -5,40 +5,6 @@ $(function () {
     var pencil = new toolPencil();
     var filler = new toolFiller();
 
-    // pixel manipulation
-    /* FINAL IMPLEMENTATION:
-    function isSameColor(img, x, y, color) {
-        var data = img.data;
-        var offset = ((y * (img.width * 4)) + (x * 4));
-        if ((data[offset + 0]) != ((color >> 24) & 0xFF)
-          || (data[offset + 1]) != ((color >> 16) & 0xFF)
-          || (data[offset + 2]) != ((color >> 8) & 0xFF)
-            //|| (data[offset + 3]) != (color & 0xFF)
-            ) {
-            return false;
-        }
-        return true;
-    }
-    
-    function getPixelColor(img, x, y) {
-        var data = img.data;
-        var offset = ((y * (img.width * 4)) + (x * 4));
-        var result = data[offset + 0] << 24; // r
-        result |= data[offset + 1] << 16; // g
-        result |= data[offset + 2] << 8; // b
-        //result |= data[offset + 3] << 0; // a
-        return result;
-    }
-    
-    function setPixelColor(img, x, y, color) {
-        var data = img.data;
-        var offset = ((y * (img.width * 4)) + (x * 4));
-        data[offset + 0] = (color >> 24) & 0xFF;
-        data[offset + 1] = (color >> 16) & 0xFF;
-        data[offset + 2] = (color >>  8) & 0xFF;
-        //data[offset + 3] = (color >>  0) & 0xFF;
-    }
-    */
     function isSameColor(img, x, y, color) {
         var data = img.data;
         var offset = ((y * (img.width * 4)) + (x * 4));
@@ -89,75 +55,29 @@ $(function () {
             var y = ev.pageY - pos.y;
             var img = context.getImageData(0, 0, W, H);
             
-            var visited = new Array(W * H); // visited pixels map
-            
             var hitColor = getPixelColor(img, x, y);
             var stack = [];
             stack.push({ x: x, y: y });
             var newColor = (intval('red') << 24) | (intval('green') << 16) | (intval('blue') << 8);
             setPixelColor(img, x, y, newColor);
-            visited[x*W + y] = true; // mark as visited
             while (stack.length > 0) {
                 var cur = stack.pop();
 
                 for (var i = 0; i < 4; i++) {
                     var pixelHash = (cur.x + dx[i])*W + cur.y + dy[i];
-                    if (cur.x + dx[i] < 0 || cur.y + dy[i] < 0 || cur.x + dx[i] >= W || cur.y + dy[i] >= H || visited[pixelHash] || !isSameColor(img, cur.x + dx[i], cur.y + dy[i], hitColor)) {
+                   if (cur.x + dx[i] < 0 || cur.y + dy[i] < 0 || cur.x + dx[i] >= W || cur.y + dy[i] >= H || !isSameColor(img, cur.x + dx[i], cur.y + dy[i], hitColor)) {
                         continue;
                     }
                     setPixelColor(img, cur.x + dx[i], cur.y + dy[i], newColor);
-                    visited[pixelHash] = true; // mark as visited
                     stack.push({ x: cur.x + dx[i], y: cur.y + dy[i]});
                 }
             }
 
             context.putImageData(img, 0, 0);
-/* FINAL IMPLEMENTATION:
-            // init
-            stopWatch.start('init');
-            var visited = new Array(W * H);
-            var canvas = document.getElementById('myCanvas');
-            var context = canvas.getContext('2d');
-            var pos = findPos(this);
-            var x = ev.pageX - pos.x;
-            var y = ev.pageY - pos.y;
-            var img = context.getImageData(0, 0, W, H);
-            var initTime = stopWatch.stop('init');
-            
-            // flood
-            stopWatch.start('flood');
-            var hitColor = getPixelColor(img, x, y);
-            var stack = [];
-            stack.push({ x: x, y: y });
-            var newColor = (intval('red') << 24) | (intval('green') << 16) | (intval('blue') << 8);
-            setPixelColor(img, x, y, newColor);
-            visited[x * W + y] = true;
-            while (stack.length > 0) {
-                var cur = stack.pop();
 
-                for (var i = 0; i < 4; i++) {
-                    var c = { x: cur.x + dx[i], y: cur.y + dy[i] };
-                    if (c.x < 0 || c.y < 0 || c.x >= W || c.y >= H || visited[c.x * W + c.y] || !isSameColor(img, c.x, c.y, hitColor)) {
-                        continue;
-                    }
-                    setPixelColor(img, c.x, c.y, newColor);
-                    visited[c.x * W + c.y] = true;
-                    stack.push(c);
-                }
-            }
-            var floodTime = stopWatch.stop('flood');
-
-            // put it back
-            stopWatch.start('putBack');
-            context.putImageData(img, 0, 0);
-            var putBackTime = stopWatch.stop('putBack');
-*/
             // measure execution time
             var time = stopWatch.stop('global');
             log('Total fill execution time: ' + time.delta() + " ms");
-            //log('Init execution time: ' + initTime.delta() + " ms");
-            //log('Flood fill execution time: ' + floodTime.delta() + " ms");
-            //log('Put data back execution time: ' + putBackTime.delta() + " ms");
         };
     }
     
